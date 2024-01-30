6802cf0790a63223ee0b1c1abcd729c1d2d1eefa
Gaja Gujt <gaja.gujt1@student.um.si>
2024-01-30 10:04:44 +0100

    aa

---
+++ backend/projekt/src/main/java/si/um/feri/rest/ProduktController.java
@@ -0,0 +1,23 @@
+package si.um.feri.rest;
+/*
+public class ProduktController {
+    public Uni <Response> putProduct(PathParam("id") int id, si.um.feri.dto.Produkt dto){
+        return produktRepository.findById((long)id)
+        .onIten().ifNull().failWith(new NotFoundException("produkt not found)
+        .onItem().invoke(produkt->produkt.updateFrom(dto))
+        .onItem().invoke(produktRepository::persist)
+        .onItem().transform(produkt->Response.ok(produkt.toDto()).build());
+        }
+}
+
+
+
+    <dependency>
+      <groupId>io.quarkus</groupId>
+      <artifactId>quarkus-hibernate-reactive-panache</artifactId>
+    </dependency>
+    <dependency>
+      <groupId>io.quarkus</groupId>
+      <artifactId>quarkus-reactive-pg-client</artifactId>
+    </dependency>
+*/

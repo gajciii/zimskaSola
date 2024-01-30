9b29b349cb5a3831a47b16306e0bca162ba27e84
Gaja Gujt <gaja.gujt1@student.um.si>
2024-01-30 14:13:50 +0100

    cipresa

--- backend/projekt/src/main/java/si/um/feri/rest/ProduktController.java
+++ backend/projekt/src/main/java/si/um/feri/rest/ProduktController.java
@@ -1,23 +1,71 @@
 package si.um.feri.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import si.um.feri.dao.ProduktRepository;
+import si.um.feri.vao.Produkt;
 /*
+@CrossOrigin
 public class ProduktController {
-    public Uni <Response> putProduct(PathParam("id") int id, si.um.feri.dto.Produkt dto){
-        return produktRepository.findById((long)id)
-        .onIten().ifNull().failWith(new NotFoundException("produkt not found)
-        .onItem().invoke(produkt->produkt.updateFrom(dto))
-        .onItem().invoke(produktRepository::persist)
-        .onItem().transform(produkt->Response.ok(produkt.toDto()).build());
-        }
-}
 
+	private static final Logger log = Logger.getLogger(si.um.feri.rest.ProduktController.class.toString());
 
+	@Autowired
+	private ProduktRepository dao;
 
-    <dependency>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-hibernate-reactive-panache</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-reactive-pg-client</artifactId>
-    </dependency>
-*/
+	@GetMapping("/products")
+	public @ResponseBody Iterable<si.um.feri.dto.<Product> getAllProducts() {
+		List<si.um.feri.dto.Product> ret=new ArrayList<>();
+		dao.findAll().forEach(p -> ret.add(p.toDto()));
+		return ret;
+	}
+	
+	@GetMapping("/products/{id}")
+	public @ResponseBody ResponseEntity<si.um.feri.dto.Produkt> getProductById(@PathVariable("id") int id) {
+		//validate
+		Optional<Product> val=dao.findById(id);
+		if (val.isEmpty()) {
+			log.info(()->"/products/"+id+" ; Product not found!");
+			return new ResponseEntity("product-not-found",HttpStatus.NOT_ACCEPTABLE);
+		}
+		return ResponseEntity.ok(val.get().toDto());
+	}
+	
+	@PostMapping("/products")
+	public ResponseEntity<si.um.feri.measurements.dto.Product> postProduct(@RequestBody si.um.feri.measurements.dto.Product pc) {
+		Product vao=dao.save(new Product(pc));
+	    return ResponseEntity.ok(vao.toDto());
+	}
+	
+	@PutMapping("/products/{id}")
+	public ResponseEntity<si.um.feri.dto.Product> putProduct(@PathVariable("id") int id, @RequestBody si.um.feri.dto.Produkt v) {
+		//validate
+		Optional<Produkt> val=dao.findById(id);
+		if (val.isEmpty()) {
+			log.info("/products/"+id+" ; Product not found!");
+			return new ResponseEntity("product-not-found",HttpStatus.NOT_ACCEPTABLE);
+		}
+				
+		Produkt vao=val.get();
+		vao.updateFrom(v);
+		vao=dao.save(vao);
+	    return ResponseEntity.ok(vao.toDto());
+	}
+	
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<String> deleteProduct(@PathVariable("id") int id) {
+		//validate
+		Optional<Produkt> val=dao.findById(id);
+		if (val.isEmpty()) {
+			log.info("/products/"+id+" ; Product not found!");
+			return new ResponseEntity("product-not-found",HttpStatus.NOT_ACCEPTABLE);
+		}
+		Produkt vao=val.get();
+		dao.delete(vao);
+	    return ResponseEntity.ok("deleted");
+	}
+
+}*/
